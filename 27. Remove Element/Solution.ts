070f6321f085fd3908e76f016e22b6b2af49d1a2
owen-gibbons <ozone8@protonmail.com>
2024-12-20 13:42:39 -0500

    Removed unncessary for-loop

--- 27. Remove Element/Solution.ts
+++ 27. Remove Element/Solution.ts
@@ -1,10 +1,7 @@
 function removeElement(nums: number[], val: number): number {
     const newArr = nums.filter(x => x !== val);
     const k = newArr.length;
-    for(let i = newArr.length-1; i < nums.length; i++) {
-        newArr.push(val);
-    }
-    for(let i = 0; i < nums.length; i++) {
+    for(let i = 0; i < newArr.length; i++) {
         nums[i] = newArr[i];
     }
     return k;
