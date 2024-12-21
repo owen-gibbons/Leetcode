8cea1f0b63d36b272fc4a5d0291278f3a5d59f7e
owen-gibbons <ozone8@protonmail.com>
2024-12-21 15:32:13 -0500

    Removed unncessary k declaration

--- 27. Remove Element/Solution.ts
+++ 27. Remove Element/Solution.ts
@@ -1,8 +1,7 @@
 function removeElement(nums: number[], val: number): number {
     const newArr = nums.filter(x => x !== val);
-    const k = newArr.length;
     for(let i = 0; i < newArr.length; i++) {
         nums[i] = newArr[i];
     }
-    return k;
+    return newArr.length;
 };
